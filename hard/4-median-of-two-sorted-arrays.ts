b0230e1e4b86126f1f854fc4672f5196b0a0760c
Lucas Hungaro <lhungaro10@gmail.com>
2025-04-09 17:58:30 -0300

    feat: add 4-median-of-two-sorted-arrays problem

---
+++ hard/4-median-of-two-sorted-arrays.ts
@@ -0,0 +1,18 @@
+
+const nums1=[1,2,3,4,5]
+const nums2=[6,7,8,9,10,11,12,13,14,15,16,17]
+
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+  const isOdd = (n:number)=> (n % 2) != 0
+  const median = (arr:number[])=>{
+      if(isOdd(arr.length)){
+        return arr[Math.trunc(arr.length / 2)]
+      }
+      
+      return (arr[arr.length / 2 -1] + arr[arr.length / 2])/2 
+  }
+  const mergedArray = [...nums1, ...nums2].sort((a, b) => a - b)
+  return median(mergedArray)
+};
+
+console.log(findMedianSortedArrays(nums1, nums2))
